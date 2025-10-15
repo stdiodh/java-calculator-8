cdb665a5f43e8f2dd11184981eebb3fe038355e0
stdiodh <playlistdh@gmail.com>
2025-10-15 14:14:11 +0900

    feat(view): 사용자 입력 기능 구현
    - 사용자의 입력을 담당하는 `InputView` 클래스 생성
    - 안내 문구 출력을 담당하는 `OutputView` 클래스 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+
+        outputView.printInputPrompt();
+        String userInput = inputView.getUserInput();
+
+        System.out.println(userInput);
+        //TODO: 문자열 분리 및 파싱
     }
 }
