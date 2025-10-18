e52150a447667905ed178252a0bbe318220bbae6
stdiodh <playlistdh@gmail.com>
2025-10-18 14:55:23 +0900

    feat(domain): 유효성 검증 기능 구현
    - `try-catch`를 사용하여 숫자가 아닌 값이 포함된 경우 예외 처리
    - 음수 값이 포함된 경우 예외 처리

--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -1,21 +1,46 @@
 package calculator.domain;
 
 public class Numbers {
+    private static final String MESSAGE_NOT_NUMBER = "숫자가 아닌 값이 포함되어 있습니다.";
+    private static final String MESSAGE_NEGATIVE = "음수는 포함될 수 없습니다.";
+
     private final int[] values;
 
     public Numbers(Tokens tokens) {
         this.values = toInts(tokens);
+        validateNegativeNumbers();
     }
 
-    public int[] toInts(Tokens tokens) {
+    private int[] toInts(Tokens tokens) {
         int length = tokens.length();
         String[] stringTokens = tokens.stream().toArray(String[]::new);
 
         int[] numbers = new int[length];
-        for (int i = 0; i < length; i++) {
+        try {
+            fillNumbers(numbers, stringTokens);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(MESSAGE_NOT_NUMBER);
+        }
+
+        return numbers;
+    }
+
+    private void fillNumbers(int[] numbers, String[] stringTokens) {
+        for (int i = 0; i < stringTokens.length; i++) {
             numbers[i] = Integer.parseInt(stringTokens[i]);
         }
-        return numbers;
+    }
+
+    private void validateNegativeNumbers() {
+        for (int number : this.values) {
+            validateSingleNumberPositive(number);
+        }
+    }
+
+    private void validateSingleNumberPositive(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(MESSAGE_NEGATIVE);
+        }
     }
 
     public int sum() {
